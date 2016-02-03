6aca599e650ec80c14ec0ddfe76e6855333c9ffa
Etienne Dumoulin <etienne.dumoulin@idiro.com>
2016-02-03 17:28:02 +0000

    Add Copyright

--- src/main/java/com/idiro/tm/task/utils/ImportPreferences.java
+++ src/main/java/com/idiro/tm/task/utils/ImportPreferences.java
@@ -1,3 +1,22 @@
+/** 
+ *  Copyright © 2016 Red Sqirl, Ltd. All rights reserved.
+ *  Red Sqirl, Clarendon House, 34 Clarendon St., Dublin 2. Ireland
+ *
+ *  This file is part of Utility for command line programmes
+ *
+ *  User agrees that use of this software is governed by: 
+ *  (1) the applicable user limitations and specified terms and conditions of 
+ *      the license agreement which has been entered into with Red Sqirl; and 
+ *  (2) the proprietary and restricted rights notices included in this software.
+ *  
+ *  WARNING: THE PROPRIETARY INFORMATION OF Utility for command line programmes IS PROTECTED BY IRISH AND 
+ *  INTERNATIONAL LAW.  UNAUTHORISED REPRODUCTION, DISTRIBUTION OR ANY PORTION
+ *  OF IT, MAY RESULT IN CIVIL AND/OR CRIMINAL PENALTIES.
+ *  
+ *  If you have received this software in error please contact Red Sqirl at 
+ *  support@redsqirl.com
+ */
+
 package com.idiro.tm.task.utils;
 
 import java.io.BufferedInputStream;
